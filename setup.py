931e840800d22959473c8aeeeece93ff21e5da8c
Alexandros Stratoudakis <alexstrat4@gmail.com>
2024-08-18 12:26:53 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
+import sys
 
+# ensure compatible python version
+if sys.version_info <= (3, 10):
+    raise Exception('Python must be at least on version 3.10.0')
 
-# find version 
+# find version
 with open('raia/__init__.py') as f:
     info = {}
     for line in f:
@@ -12,13 +16,15 @@ with open('raia/__init__.py') as f:
 
 setup_info = dict(
     name='raia',
+    python_requires=">=3.10",
     version=info['__version__'],
     author='Alexandros Stratoudakis',
     long_description_content_type='text/markdown',
-    long_description = open('README.md','r').read(),
+    long_description=open('README.md', 'r').read(),
     author_email='alexstrat4@gmail.com',
     url='https://github.com/AlexStratou/raia',
-    download_url='https://github.com/AlexStratou/raia/archive/refs/tags/v0.1.0.zip',
+    download_url='https://github.com/AlexStratou/raia/archive/refs/tags/v' +
+        info['__version__']+'.zip',
     description='Simplistic python package to print colored and/or styled text with a user friendly API.',
     license='MIT',
     classifiers=[
@@ -27,16 +33,14 @@ setup_info = dict(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 
     # Package info
-    packages =  find_packages()
+    packages=find_packages()
 
 )
 
