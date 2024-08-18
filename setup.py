e3f0a57039932337a12cc70819daf8af9b702816
Alexandros Stratoudakis <alexstrat4@gmail.com>
2024-08-18 10:42:03 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup_info = dict(
     name='raia',
     version=info['__version__'],
     author='Alexandros Stratoudakis',
+    long_description_content_type='text/markdown',
     author_email='alexstrat4@gmail.com',
     url='https://github.com/AlexStratou/raia',
     download_url='https://github.com/AlexStratou/raia/archive/refs/tags/v0.1.0.zip',
